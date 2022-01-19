24333c947b84467faf71a4f8f52ee626f324128b
gokcelb <gokcelbilgin@gmail.com>
2022-01-19 19:20:19 +0300

    Fix bugs, Randomize bet logic

--- ai.py
+++ ai.py
@@ -1,17 +1,18 @@
+import random
 from player import Player
 
 
 class AI(Player):
     instances = 0
 
-    def __init__(self, hand, funds):
-        super().__init__(hand, funds)
+    def __init__(self):
+        super().__init__()
         AI.instances += 1
         self.name = f"player{str(AI.instances)}"
 
     def act(self, dealer_hand):
         has_ace = False
-        for card in self.hand:
+        for card in self.hand.hand:
             if type(card.get_score()) is dict:
                 has_ace = True
 
@@ -19,33 +20,28 @@ class AI(Player):
         if has_ace:
             hand_type = "soft"
 
-        self.check_hand(hand_type, dealer_hand)
+        return self.check_hand(hand_type, dealer_hand)
 
-    def check_hand(self, hand_type, dealer_hand):
-        if 2 <= dealer_hand.calculate_score() <= 6:
+    def check_hand(self, hand_type, dealer_hand_score):
+        if 2 <= dealer_hand_score <= 6:
             return self.low_dealer_hand(hand_type)
         else:
             return self.high_dealer_hand(hand_type)
 
     def low_dealer_hand(self, hand_type):
-        score = self.hand.calculate_score()
+        score = self.hand.score
         if hand_type == "hard":
             return "hit" if 4 <= score <= 11 else "stay"
         else:
             return "hit" if 13 <= score <= 18 else "stay"
 
     def high_dealer_hand(self, hand_type):
-        score = self.hand.calculate_score()
+        score = self.hand.score
         if hand_type == "hard":
             return "hit" if 4 <= score <= 16 else "stay"
         else:
             return "hit" if 13 <= score <= 18 else "stay"
 
     def bet(self):
-        if 0 < self.funds < 800:
-            return self.funds / 2
-        elif 800 <= self.funds <= 1500:
-            return self.funds
-        else:
-            return self.funds / 3 * 2
-        
+        coefficients = [1, 1/2, 1/3, 1/4, 1/5, 2/3, 3/4, 2/5, 3/5, 4/5]
+        return self.funds * random.choice(coefficients)
