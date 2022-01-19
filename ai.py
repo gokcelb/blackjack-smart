9d790ad040bd3e4b91d98fcdd2ae421b0f4a842e
gokcelb <gokcelbilgin@gmail.com>
2022-01-19 15:47:41 +0300

    Code AI class logic

--- ai.py
+++ ai.py
@@ -2,11 +2,50 @@ from player import Player
 
 
 class AI(Player):
+    instances = 0
+
     def __init__(self, hand, funds):
         super().__init__(hand, funds)
+        AI.instances += 1
+        self.name = f"player{str(AI.instances)}"
 
-    def act(self):
-        pass
+    def act(self, dealer_hand):
+        has_ace = False
+        for card in self.hand:
+            if type(card.get_score()) is dict:
+                has_ace = True
+
+        hand_type = "hard"
+        if has_ace:
+            hand_type = "soft"
+
+        self.check_hand(hand_type, dealer_hand)
+
+    def check_hand(self, hand_type, dealer_hand):
+        if 2 <= dealer_hand.calculate_score() <= 6:
+            return self.low_dealer_hand(hand_type)
+        else:
+            return self.high_dealer_hand(hand_type)
+
+    def low_dealer_hand(self, hand_type):
+        score = self.hand.calculate_score()
+        if hand_type == "hard":
+            return "hit" if 4 <= score <= 11 else "stay"
+        else:
+            return "hit" if 13 <= score <= 18 else "stay"
+
+    def high_dealer_hand(self, hand_type):
+        score = self.hand.calculate_score()
+        if hand_type == "hard":
+            return "hit" if 4 <= score <= 16 else "stay"
+        else:
+            return "hit" if 13 <= score <= 18 else "stay"
 
     def bet(self):
-        pass
+        if 0 < self.funds < 800:
+            return self.funds / 2
+        elif 800 <= self.funds <= 1500:
+            return self.funds
+        else:
+            return self.funds / 3 * 2
+        
--- player.py
+++ player.py
@@ -5,6 +5,7 @@ class Player:
     def __init__(self):
         self.hand = Hand()
         self.funds = 500
+        self.name = "You"
 
     def __str__(self):
         return f"${self.funds}"
